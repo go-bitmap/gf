6e84c8a0e907c9d13d676a7f52c7af82e0beaa5f
it0001hot <liu15077731547@gmail.com>
2024-07-22 11:38:07 +0800

    feat: delete metadata

--- contrib/rpc/grpcx/grpcx_grpc_register.go
+++ contrib/rpc/grpcx/grpcx_grpc_register.go
@@ -4,7 +4,7 @@ import (
 	"context"
 )
 
-func (s *GrpcServer) RegisterWithMetadata(ctx context.Context, key string, value interface{}) {
+func (s *GrpcServer) SetMetadata(ctx context.Context, key string, value interface{}) {
 	if s.registrar == nil {
 		return
 	}
@@ -19,6 +19,33 @@ func (s *GrpcServer) RegisterWithMetadata(ctx context.Context, key string, value
 	// Register service list after server starts.
 	for i, service := range s.services {
 		service.GetMetadata().Set(key, value)
+		s.Logger().Debugf(ctx, `service register: %+v`, service)
+		if len(service.GetEndpoints()) == 0 {
+			s.Logger().Warningf(ctx, `no endpoints found to register service, abort service registering`)
+			return
+		}
+		if service, err = s.registrar.Register(ctx, service); err != nil {
+			s.Logger().Fatalf(ctx, `%+v`, err)
+		}
+		s.services[i] = service
+	}
+}
+
+func (s *GrpcServer) DelMetadata(ctx context.Context, key string) {
+	if s.registrar == nil {
+		return
+	}
+	s.serviceMu.Lock()
+	defer s.serviceMu.Unlock()
+	if len(s.services) == 0 {
+		return
+	}
+	var (
+		err error
+	)
+	// Register service list after server starts.
+	for i, service := range s.services {
+		service.GetMetadata().Del(key)
 		s.Logger().Debugf(ctx, `service register: %+v`, service)
 		if len(service.GetEndpoints()) == 0 {
 			s.Logger().Warningf(ctx, `no endpoints found to register service, abort service registering`)
